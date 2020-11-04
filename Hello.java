9819588c267c33799fcbe67c3a2f77451b992134
iyoons <han3338@hansung.ac.kr>
2020-11-04 12:50:13 +0900

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
-        public static void main(String[] args) {
-                System.out.println("Hello, world from GiT!");
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello, world from GiT!");
+        System.out.println("Changes after First commit!");
+	}
 }
